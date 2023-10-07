c4d9ceabe21a51f57e68d02db423176bfff20e73
Aparna-200 <142496437+Aparna-200@users.noreply.github.com>
2023-10-07 19:53:33 +0530

    a1_q1.java
    ln this question we have to print the letter z .

---
+++ a1_q1.java
@@ -0,0 +1,16 @@
+// Assignment 1 - Question 1
+public class first {
+    public static void main(String[] args) {
+
+        System.out.println("********");
+        System.out.println("      * ");
+        System.out.println("     *  ");   
+        System.out.println("    *   ");
+        System.out.println("   *    ");    
+        System.out.println("  *     ");    
+        System.out.println(" *      ");
+        System.out.println("********");
+                        
+
+    }  
+}
